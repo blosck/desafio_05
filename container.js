d8561115ab7fe6d2daa57bbc15507973bbd96459
Pablo Perez <pablo.ppinto@outlook.com>
2022-07-21 21:45:53 -0400

    first commit

--- container.js
+++ container.js
@@ -7,6 +7,7 @@ const contenedor = new Contenedor('productos.txt'); //Dejar el punto y coma al f
 
 (async function() {
     //await contenedor.init()
+    //await contenedor.init()
     //await contenedor.save({title: 'Arroz', price: 200, thumbnail: "https://tumarchante.mx/wp-content/uploads/2021/03/arroz-sos-grueso-super-extra-1-kg-e1604468251675-1.jpg"})
     //await contenedor.save({title: 'Fideos', price: 300, thumbnail: "https://th.bing.com/th/id/OIP.IqSVK7IP84kuyNzbtyE-wQHaHa?pid=ImgDet&rs=1"})
     //await contenedor.save({title: 'Puré', price: 100, thumbnail: "https://th.bing.com/th/id/OIP.HjKHD9Cnkddibfyi5G7rVQHaHa?pid=ImgDet&rs=1"})
